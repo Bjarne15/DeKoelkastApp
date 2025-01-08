c5fbb1b6bc19a6ad6d2906ab9837fc1464ebe92d
Bjarne Asch <2404070asch@zuyd.nl>
2025-01-08 14:28:49 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DeKoelkastApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
